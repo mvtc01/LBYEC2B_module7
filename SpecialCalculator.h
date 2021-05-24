e127fd39f812c4dd9fd63760cd4e9ca8ec9cbffe
Maria Victoria Ching <70054433+mvtc01@users.noreply.github.com>
2021-05-24 15:28:43 +0800

    Update SpecialCalculator.h

--- SpecialCalculator.h
+++ SpecialCalculator.h
@@ -1,16 +1,7 @@
-
+#include "Calculator.h"
 #ifndef __SPECIALCALCULATOR_H__
 #define __SPECIALCALCULATOR_H__
 
-class Calculator
-{
-public:
-    Calculator();
-    int addition(int, int);
-    int subtraction(int, int);
-    int multiplication(int, int);
-    float division(int, int);
-};
 
 class CalculatorWithMod: public Calculator{
 public:
