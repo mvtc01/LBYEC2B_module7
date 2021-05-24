873bce9c2da88387761917a29ea39168e6591cd4
Maria Victoria Ching <70054433+mvtc01@users.noreply.github.com>
2021-05-24 12:09:48 +0800

    Update SpecialCalculator.h

--- SpecialCalculator.h
+++ SpecialCalculator.h
@@ -1,13 +1,14 @@
-#ifndef __CALCULATOR_H__
-#define __CALCULATOR_H__
 
-/**SpecialCalculator.h
- * Create the header file for SpecialCalculator here
- * Do not write the implementation. It should be done on cpp file.
- * For reference: please check calculator.hpp and calculator.cpp
- * Make sure to write your code inside the header guards.
-*/
+#ifndef __SPECIALCALCULATOR_H__
+#define __SPECIALCALCULATOR_H__
 
+
+class CalculatorWithMod: public Calculator{
+public:
+    CalculatorWithMod();
+    int modulo(int, int);
+};
 
 
 #endif
+
