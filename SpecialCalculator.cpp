2a1cedc99b8a1501d19097b8d5611b7365b96eca
Maria Victoria Ching <70054433+mvtc01@users.noreply.github.com>
2021-05-24 12:09:22 +0800

    Update SpecialCalculator.cpp

--- SpecialCalculator.cpp
+++ SpecialCalculator.cpp
@@ -1,4 +1,11 @@
-/**SpecialCalculator.cpp
-*/
 
 #include "SpecialCalculator.h"
+#include <iostream>
+
+int CalculatorWithMod::modulo(int a, int b)
+{
+    if (b == 0)
+        throw "Division by zero!";
+
+    return (a % b);
+}
